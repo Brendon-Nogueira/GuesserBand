2d391d77f32ca160baad174133b8695e668f734a
Brendon-Nogueira <brendonnogueira21@gmail.com>
2025-11-14 13:01:27 -0300

    t

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   // base: "/guess-the-band/",
   plugins: [react()],
   // server: {
