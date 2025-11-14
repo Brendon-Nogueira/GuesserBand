9f99ee3c8cb7be8a44cac1512ece5e8aff1b0ee8
Brendon-Nogueira <brendonnogueira21@gmail.com>
2025-11-14 11:16:09 -0300

    feat: add server proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,13 @@ export default defineConfig({
   base: "/",
   //base: "/guess-the-band/",
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
